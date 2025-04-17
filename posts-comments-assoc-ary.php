583de12aa01fafbd1e665edf40394930313cb49e
Your Name <you@example.com>
2025-04-17 11:46:41 +0300

    DB pieslēgums un tabulu izveide ar datiem

---
+++ posts-comments-assoc-ary.php
@@ -0,0 +1,6 @@
+<?php
+// create database blog_17042025;
+// use blog_17042025;
+// create user sandis_17042025@localhost identified by 'password';
+// grant all privileges  on blog_17042025.* to sandis_17042025@localhost;
+
