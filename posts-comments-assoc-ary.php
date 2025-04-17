fc5d6d74d838d426ea82230e535fd7a3cbc1fa37
Your Name <you@example.com>
2025-04-17 12:12:04 +0300

    Algoritms: no plakana uz hierarhisku masīvu

--- posts-comments-assoc-ary.php
+++ posts-comments-assoc-ary.php
@@ -21,4 +21,49 @@ try {
 $stmt = $pdo->query("SELECT * FROM posts");
 $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$sql = "
+    SELECT
+        p.id AS post_id,
+        p.title AS post_title,
+        p.content AS post_content,
+        c.id AS comment_id,
+        c.author AS comment_author,
+        c.content AS comment_content
+    FROM posts p
+    LEFT JOIN comments c ON p.id = c.post_id
+    ORDER BY p.id, c.id
+";
+$flatData = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+
+$hierarchy = [];
+
+foreach ($flatData as $row) {
+    $postId = $row['post_id'];
+
+    if (!isset($hierarchy[$postId])) {
+        $hierarchy[$postId] = [
+            'title' => $row['post_title'],
+            'content' => $row['post_content'],
+            'comments' => []
+        ];
+    }
+
+    if (!empty($row['comment_id'])) {
+        $hierarchy[$postId]['comments'][] = [
+            'author' => $row['comment_author'],
+            'content' => $row['comment_content']
+        ];
+    }
+}
+
+
+
+
+
+
+
+
+
+
   ?>
