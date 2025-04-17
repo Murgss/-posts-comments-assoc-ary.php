3634ec238fb635ee6e41a50f10bec57f44fc1c6d
Your Name <you@example.com>
2025-04-17 12:04:09 +0300

    Atlase no posts bez hierarhijas

--- posts-comments-assoc-ary.php
+++ posts-comments-assoc-ary.php
@@ -4,3 +4,7 @@
 // create user sandis_17042025@localhost identified by 'password';
 // grant all privileges  on blog_17042025.* to sandis_17042025@localhost;
 
+$host = 'localhost';
+$dbname = 'blog_17042025';
+$username = 'sandis_17042025';
+$password = 'password';
